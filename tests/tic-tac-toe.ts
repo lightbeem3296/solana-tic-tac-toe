f3a482a56595e50a2efa524e467fa9426c3e5351
Billy Lights <alpha5611331@gmail.com>
2024-06-05 06:01:56 -0400

    Done Setup Game

--- tests/tic-tac-toe.ts
+++ tests/tic-tac-toe.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { TicTacToe } from "../target/types/tic_tac_toe";
+import { expect } from "chai";
 
 describe("tic-tac-toe", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +9,28 @@ describe("tic-tac-toe", () => {
 
   const program = anchor.workspace.TicTacToe as Program<TicTacToe>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("setup game!", async () => {
+    const gameKeypair = anchor.web3.Keypair.generate()
+    const playerOne = (program.provider as anchor.AnchorProvider).wallet
+    const playerTwo = anchor.web3.Keypair.generate()
+    await program.methods
+      .setupGame(playerTwo.publicKey)
+      .accounts({
+        game: gameKeypair.publicKey,
+        playerOne: playerOne.publicKey,
+      }).signers([gameKeypair])
+      .rpc()
+
+    let gameState = await program.account.game.fetch(gameKeypair.publicKey)
+    console.log(gameState)
+
+    expect(gameState.turn).to.equal(1)
+    expect(gameState.players).to.eql([playerOne.publicKey, playerTwo.publicKey])
+    expect(gameState.state).to.eql({ active: {} })
+    expect(gameState.board).to.eql([
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ])
   });
 });
